de166a04acbb490e7bb21b723d8f097859ada1fa
=Rahul lodhi <146419777+RAHUL676789@users.noreply.github.com>
2025-10-13 12:29:42 +0530

    HomeDash

--- Backend/controllers/HomeDash/homeDash.js
+++ Backend/controllers/HomeDash/homeDash.js
@@ -6,7 +6,13 @@ const Partner = require("../../models/partnerSchema");
 
 module.exports.getHomeDash = async (req,res,next) => {
 
-    const recentBookings = await Bookings.find().sort({createdAt:-1}).limit(10);
+    const recentBookings = await Bookings.find().sort({createdAt:-1}).limit(10).populate("service").populate({
+        path:"user",
+        select:"-password -bookings "
+    }).populate({
+        path:"provider",
+        select:"-password -bookings -backGroundImage -services -address -documents "
+    });
     const totalPartners = await Partner.countDocuments();
     const totalBookings = await Bookings.countDocuments();
     const completedBookings = await Bookings.countDocuments({status:"accepted"});
